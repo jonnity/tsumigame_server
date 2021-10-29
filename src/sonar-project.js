c8569ab59a2e328ae6a93d205be6da3431cbc84b
takahashi <takahashi.yuto2021@gmail.com>
2021-10-29 12:56:14 +0900

    やることはこんな感じ、あとは環境変数

---
+++ src/sonar-project.js
@@ -0,0 +1,24 @@
+const sonarqubeScanner = require("sonarqube-scanner");
+
+const SONARQUBE_URL = "https://sonarcloud.io/";
+const SONARQUBE_TOKEN = process.argv[2];
+
+const PROJECT_NAME = "tsumigame_server";
+sonarqubeScanner(
+  {
+    serverUrl: SONARQUBE_URL,
+    options: {
+      "sonar.sourceEncoding": "UTF-8",
+      "sonar.organization": "jonnity",
+      "sonar.projectKey": PROJECT_NAME,
+      "sonar.sources": ".",
+      "sonar.login": SONARQUBE_TOKEN,
+      "sonar.projectName": PROJECT_NAME,
+      "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
+      "sonar.tests": "tests/",
+      "sonar.exclusions": "./tests/**, sonar-project.js, .prettierrc.js",
+      "sonar.test.inclusions": "./tests/**.test.ts",
+    },
+  },
+  () => {}
+);
